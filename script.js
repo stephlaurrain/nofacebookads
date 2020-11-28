2770549fa1fd6376b525c87ba64172a85e3b3a55
lor1cd <laurrains@gmail.com>
2020-11-28 16:53:57 +0100

    update suggestions

--- script.js
+++ script.js
@@ -2,43 +2,51 @@
 // @name         NoFacebookAds
 // @namespace    https://tampermonkey.net/
 // @version      1.666
-// @description  Hide sponsored articles on Facebook
-// @author       Golgoth
+// @description  Hide sponsored and suggeste darticles on Facebook
+// @author       BlackHoleSon
 // @match        https://www.facebook.com/*
 // @license MIT
 // @grant        none
 // ==/UserScript==
 
 const HideAds = () => {
- try {
-  const maindiv = document.querySelector('div[role="feed"]');
-    var feeds = maindiv.querySelectorAll('div[data-pagelet*="FeedUnit_"');
-
-  feeds.forEach(feed => {
-
-     if(feed.style.display != "none"){
-         try {
-            var sponsored = feed.querySelector('a[aria-label="Sponsorisé"]');
-            if (sponsored!==null) {
-                feed.style.display = "none";
-            }
-         }
-         catch (e) {
-         }
-    }
-  });
+  try {
+   const maindiv = document.querySelector('div[role="feed"]');
+     var feeds = maindiv.querySelectorAll('div[data-pagelet*="FeedUnit_"');
+ 
+   feeds.forEach(feed => {
+ 
+      if(feed.style.display != "none"){
+          try {
+             var sponsored = feed.querySelector('a[aria-label="Sponsorisé"]');
+             if (sponsored!==null) {
+                 feed.style.display = "none";
+             }
+             var suggesteds = feed.querySelectorAll('span[dir="auto"]');
+             suggesteds.forEach(suggested =>{
+                     if (suggested.textContent.includes("Suggestion")){
+                         feed.style.display = "none";
+ 
+                     }
+ 
+                 });
+          }
+          catch (e) {
+          }
+     }
+   });
+  }
+  catch (e) {
+  }
  }
- catch (e) {
- }
-}
-
-HideAds()
-setTimeout(HideAds, 1000);
-(function () {
-  window.addEventListener('scroll', () => {
-    HideAds()
-    setTimeout(HideAds, 1000);
-  });
-
-
-})();
+ 
+ HideAds()
+ setTimeout(HideAds, 1000);
+ (function () {
+   window.addEventListener('scroll', () => {
+     HideAds()
+     setTimeout(HideAds, 1000);
+   });
+ 
+ 
+ })();
